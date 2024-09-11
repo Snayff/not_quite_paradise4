e9521324c0a6c178c10b08b037a12646a8d6d35e
Snayff <snaith2@gmail.com>
2024-09-11 16:26:56 +0100

    added mine sprite_frame resource, projectile data class

---
+++ scenes/abcs/projectile.gd
@@ -0,0 +1,40 @@
+## abc for delivery of effects and functionality.
+#@icon("")
+class_name ABCProjectile
+extends AnimatedSprite2D
+
+
+#region SIGNALS
+
+#endregion
+
+
+#region ON READY (for direct children only)
+
+#endregion
+
+
+#region EXPORTS
+# @export_group("Component Links")
+# @export var
+#
+# @export_group("Details")
+#endregion
+
+
+#region VARS
+
+#endregion
+
+
+#region FUNCS
+
+
+
+
+
+
+
+
+
+#endregion
---
+++ scripts/dataclasses/data_projectile.gd
@@ -0,0 +1,40 @@
+## class desc
+#@icon("")
+#class_name XXX
+extends Node
+
+
+#region SIGNALS
+
+#endregion
+
+
+#region ON READY (for direct children only)
+
+#endregion
+
+
+#region EXPORTS
+# @export_group("Component Links")
+# @export var 
+#
+# @export_group("Details")
+#endregion
+
+
+#region VARS
+
+#endregion
+
+
+#region FUNCS
+
+
+
+
+
+
+
+
+
+#endregion
